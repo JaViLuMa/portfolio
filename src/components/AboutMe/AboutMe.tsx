69fd61cf788810ece222e48c52488fa865ca3e3e
Matej Ruzic <matej.gamer01@gmail.com>
2023-02-26 16:43:08 +0100

    Decrease padding

--- src/components/AboutMe/AboutMe.tsx
+++ src/components/AboutMe/AboutMe.tsx
@@ -17,7 +17,7 @@ const AboutMe: FC<AboutMeProps> = ({ data: aboutMeData, error }) => (
         className="relative z-10 mb-2 border-b-[10px] border-solid border-black pb-2 text-6xl font-bold leading-none before:absolute before:top-[3px] before:z-[-1] before:content-['About_me'] before:text-stroke-before after:absolute after:top-[8px] after:left-0 after:z-[-2] after:content-['About_me'] after:text-stroke-after xl:text-[86px]"
       />
     </Header>
-    <main className="flex h-96 flex-col items-center overflow-y-scroll border border-black px-12 pt-3 pb-6 grid-in-about-me scrollbar scrollbar-thumb-black dark:border-white dark:scrollbar-track-black dark:scrollbar-thumb-white md:py-7 md:px-4 xl:h-auto xl:py-7">
+    <main className="flex h-96 flex-col items-center overflow-y-scroll border border-black pl-12 pr-8 pt-3 pb-6 grid-in-about-me scrollbar scrollbar-thumb-black dark:border-white dark:scrollbar-track-black dark:scrollbar-thumb-white md:py-7 md:px-4 xl:h-auto xl:py-7">
       <div className="h-full pr-8 scrollbar scrollbar-thumb-black dark:scrollbar-thumb-white">
         <ReactMarkdown
           key={aboutMeData[0]?.id}
